b95ace49b2abe11e5568e6561a82bf5e471b4612
dclinton@gmail.com <dclinton@gmail.com@d718d8bd-922b-0410-8b2e-699bb0efa576>
2007-09-10 00:37:42 +0000

    Starting to add Api method signatures.

--- trunk/src/net/unto/twitter/Api.java
+++ trunk/src/net/unto/twitter/Api.java
@@ -1,6 +1,26 @@
 package net.unto.twitter;
 
+import java.util.Date;
+import java.util.Map;
+
 public class Api
 {
+  private String username;
+  private String password;
+  private Map<String, String> requestHeaders;
 
+  public Api(String username, String password, Map<String, String> requestHeaders) {
+    this.username = username;
+    this.password = password;
+    this.requestHeaders = requestHeaders;
+  }
+  
+  public Status[] getFriendsTimeline(String user, Date since) {
+    return null;
+  }
+  
+  public Status[] getUserTimeline(String user, Date since, int count) {
+    return null;
+    
+  }
 }
