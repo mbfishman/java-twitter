22b205363d9b273fa8f9fa5ae66694de3cdb2af6
dclinton <dclinton@gmail.com>
2007-09-15 15:52:08 +0000

    Mark certain dependencies runtime only.  Added more Api methods.

--- src/main/java/net/unto/twitter/DirectMessage.java
+++ src/main/java/net/unto/twitter/DirectMessage.java
@@ -1,5 +1,9 @@
 package net.unto.twitter;
 
+/*
+ [{"sender_screen_name":"mfagan","recipient_id":673483,"sender":{"url":"http:\/\/faganm.com\/","name":"mfagan","screen_name":"mfagan","description":"","profile_image_url":"http:\/\/assets2.twitter.com\/system\/user\/profile_image\/677403\/normal\/me_with_hat.jpg?1171966071","location":"Canada","id":677403,"protected":false},"created_at":"Mon Jan 22 01:52:35 +0000 2007","recipient_screen_name":"dewitt","recipient":{"url":"http:\/\/unto.net\/","name":"DeWitt","screen_name":"dewitt","description":"Indeterminate things","profile_image_url":"http:\/\/assets0.twitter.com\/system\/user\/profile_image\/673483\/normal\/me.jpg?1171965914","location":"San Francisco, CA","id":673483,"protected":false},"text":"1) I read your post on twitter\r\n2) I figure i may as well get an account\r\n3) about an hour later you've added me as a friend...  that shouldn't even be possible","sender_id":677403,"id":205913}]
+ */
+
 public class DirectMessage
 {
 
