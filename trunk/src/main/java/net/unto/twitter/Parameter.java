f9f71e3ef87df80b1e713ea71e19e55141472af3
dclinton@gmail.com <dclinton@gmail.com@d718d8bd-922b-0410-8b2e-699bb0efa576>
2007-09-29 15:44:18 +0000

    Begin fleshing out the testing classes for ApiTest.

--- trunk/src/main/java/net/unto/twitter/Parameter.java
+++ trunk/src/main/java/net/unto/twitter/Parameter.java
@@ -1,5 +1,10 @@
 package net.unto.twitter;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
 final class Parameter
 {
   private String name = null;
@@ -25,5 +30,20 @@ final class Parameter
   
   public boolean hasValue() {
     return value != null;
+  }
+  
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+  }
+
+  @Override
+  public boolean equals(Object obj) { 
+    return EqualsBuilder.reflectionEquals(this, obj);
+  }
+
+  @Override
+  public int hashCode() {
+    return HashCodeBuilder.reflectionHashCode(this);
   }
 }
--- trunk/src/test/java/net/unto/twitter/ApiTest.java
+++ trunk/src/test/java/net/unto/twitter/ApiTest.java
@@ -2,12 +2,30 @@ package net.unto.twitter;
 
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.CharBuffer;
+
+import org.easymock.EasyMock;
 import org.junit.Test;
 
 public class ApiTest {
 
   @Test
-  public void testGetPublicTimeline() throws TwitterException {
+  public void testGetPublicTimeline() throws TwitterException, IOException {
+    Api api = new Api();
+    TwitterHttpManager mockTwitterHttpManager = getMockTwitterHttpManager();
+    String json = readTestData("public-timeline.json");
+    String url = "http://twitter.com/statuses/public_timeline.json";
+    Parameter[] parameters = new Parameter[] {new Parameter("since_id", null)};
+    EasyMock.expect(mockTwitterHttpManager.get(EasyMock.eq(url), EasyMock.aryEq(parameters))).andReturn(json);
+    EasyMock.replay(mockTwitterHttpManager);
+    api.setTwitterHttpManager(mockTwitterHttpManager);
+    Status[] statuses = api.getPublicTimeline();
+    assert(10 == statuses.length);
+    EasyMock.verify(mockTwitterHttpManager);
   }
 
   @Test
@@ -40,4 +58,23 @@ public class ApiTest {
     fail("Not yet implemented");
   }
 
+  private String TEST_DATA_DIR = "src/test/data/";
+  
+  private int MAX_TEST_DATA_FILE_SIZE = 16384;
+  
+  private String readTestData(String fileName) throws IOException {
+    return readFromFile(new File(TEST_DATA_DIR, fileName));
+  }
+  
+  private String readFromFile(File file) throws IOException {
+    Reader reader = new FileReader(file);
+    CharBuffer charBuffer = CharBuffer.allocate(MAX_TEST_DATA_FILE_SIZE);
+    reader.read(charBuffer);
+    charBuffer.position(0);
+    return charBuffer.toString();
+  }
+  
+  private TwitterHttpManager getMockTwitterHttpManager() {
+    return EasyMock.createMock(TwitterHttpManager.class);
+  }
 }
