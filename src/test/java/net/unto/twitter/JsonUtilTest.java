f8ee3dd3c568f559c9a29f6910865ae860c28582
dclinton <dclinton@gmail.com>
2007-09-29 15:44:18 +0000

    Begin fleshing out the testing classes for ApiTest.

--- src/test/java/net/unto/twitter/JsonUtilTest.java
+++ src/test/java/net/unto/twitter/JsonUtilTest.java
@@ -2,19 +2,21 @@ package net.unto.twitter;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.CharBuffer;
+
 import org.junit.Test;
 
 public class JsonUtilTest {
 
-  private static String STATUS_JSON_STRING = "{\"created_at\": \"Fri Jan 26 23:17:14 +0000 2007\","
-      + " \"id\": 4391023, \"relative_created_at\": \"about 10 minutes ago\", "
-      + "\"text\": \"Canvas. JC Penny. Three ninety-eight.\", \"user\": "
-      + "{\"description\": \"Canvas. JC Penny. Three ninety-eight.\", \"id\": "
-      + "718443, \"location\": \"Okinawa, Japan\", \"name\": \"Kesuke Miyagi\","
-      + " \"profile_image_url\": \"http:\\/\\/twitter.com\\/system\\/user\\/pro"
-      + "file_image\\/718443\\/normal\\/kesuke.png\", \"screen_name\": "
-      + "\"kesuke\", \"url\": \"http:\\/\\/twitter.com\\/kesuke\"}}";
-
+  private String TEST_DATA_DIR = "src/test/data/";
+  
+  private int MAX_TEST_DATA_FILE_SIZE = 16384;
+  
   @Test
   public void testNewStatusArrayString() {
     // Status[] statuses = JsonUtil.newStatusArray(jsonString);
@@ -26,9 +28,9 @@ public class JsonUtilTest {
   }
 
   @Test
-  public void testNewStatusString() throws TwitterException {
-    Status status = JsonUtil.newStatus(STATUS_JSON_STRING);
-    assertEquals(4391023L, (long)status.getId());
+  public void testNewStatusString() throws TwitterException, IOException {
+    Status status = JsonUtil.newStatus(readTestData("status-271680732.json"));
+    assertEquals(271680732, (long)status.getId());
   }
 
   @Test
@@ -54,6 +56,18 @@ public class JsonUtilTest {
   @Test
   public void testNewUserJSONObject() {
     fail("Not yet implemented");
+  }
+  
+  private String readTestData(String fileName) throws IOException {
+    return readFromFile(new File(TEST_DATA_DIR, fileName));
+  }
+  
+  private String readFromFile(File file) throws IOException {
+    Reader reader = new FileReader(file);
+    CharBuffer charBuffer = CharBuffer.allocate(MAX_TEST_DATA_FILE_SIZE);
+    reader.read(charBuffer);
+    charBuffer.position(0);
+    return charBuffer.toString();
   }
 
 }
