91068995bfa4baa2082385d923645572bb344d6a
dclinton@gmail.com <dclinton@gmail.com@d718d8bd-922b-0410-8b2e-699bb0efa576>
2007-10-03 15:40:14 +0000

    Refactored the factory methods out of JsonUtil and into User, Status, and DirectMessage directly.  Removed dead code.

--- trunk/src/main/java/net/unto/twitter/TwitterException.java
+++ trunk/src/main/java/net/unto/twitter/TwitterException.java
@@ -1,13 +1,28 @@
 package net.unto.twitter;
 
+/**
+ * An exception wrapper for the Twitter API library.
+ * 
+ * @author dewitt@google.com
+ */
 public class TwitterException extends Exception {
 
   private static final long serialVersionUID = -7004865779218982263L;
 
+  /**
+   * Construct a new TwitterException
+   * 
+   * @param string the error message
+   */
   public TwitterException(String string) {
     super(string);
   }
 
+  /**
+   * Construct a new TwitterException.
+   * 
+   * @param e the existing exception to wrap
+   */
   public TwitterException(Exception e) {
     super(e);
   }
