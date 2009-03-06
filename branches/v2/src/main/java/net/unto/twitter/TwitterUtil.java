5ae3e752352bdafa95b36bec808b0b3681a76569
dclinton <dclinton@d718d8bd-922b-0410-8b2e-699bb0efa576>
2009-03-06 17:25:30 +0000

    Reformatted.

--- branches/v2/src/main/java/net/unto/twitter/TwitterUtil.java
+++ branches/v2/src/main/java/net/unto/twitter/TwitterUtil.java
@@ -4,16 +4,17 @@ import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
-final class TwitterUtil
-{
-  public final static DateTimeFormatter TWITTER_DATE_FORMATTER = DateTimeFormat.forPattern("EEE MMM dd HH:mm:ss Z yyyy");
-  
-  public final static DateTime parseTwitterDateTimeString(String twitterDateString) {
+final class TwitterUtil {
+  public final static DateTimeFormatter TWITTER_DATE_FORMATTER = DateTimeFormat
+      .forPattern("EEE MMM dd HH:mm:ss Z yyyy");
+
+  public final static DateTime parseTwitterDateTimeString(
+      String twitterDateString) throws TwitterException {
     try {
       return TWITTER_DATE_FORMATTER.parseDateTime(twitterDateString);
     } catch (IllegalArgumentException e) {
-      System.err.println(String.format("Could not parse date string '%s'", twitterDateString));
-      return null;
+      throw new TwitterException(String.format("Could not parse date string '%s'",
+          twitterDateString));
     }
   }
 }
