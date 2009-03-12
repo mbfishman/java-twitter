5de4662dfa7a1fc2332b5440c0544d77ef581cf9
dclinton <dclinton@d718d8bd-922b-0410-8b2e-699bb0efa576>
2009-03-12 16:38:40 +0000

    Added input validation for setting profile colors.

--- branches/v2/src/main/java/net/unto/twitter/methods/UpdateProfileColorsRequest.java
+++ branches/v2/src/main/java/net/unto/twitter/methods/UpdateProfileColorsRequest.java
@@ -2,10 +2,14 @@ package net.unto.twitter.methods;
 
 import net.unto.twitter.JsonUtil;
 import net.unto.twitter.TwitterProtos.User;
-import net.unto.twitter.methods.FriendsTimelineRequest.Builder;
+
+import java.util.regex.Pattern;
 
 
 public final class UpdateProfileColorsRequest extends AbstractRequest {
+
+  static final Pattern VALID_HEXCOLOR_PATTERN = Pattern
+      .compile("(\\p{XDigit}{3})|(\\\\p{XDigit}{6})");
 
   public static Builder builder() {
     return new Builder();
@@ -22,32 +26,64 @@ public final class UpdateProfileColorsRequest extends AbstractRequest {
       authorizationRequired(true);
     }
 
+    private boolean hasValid = false;
+
+    private void validateHexColor(String hexColor) {
+      if (isValidHexColor(hexColor)) {
+        hasValid = true;
+      } else {
+        throw new IllegalArgumentException(String.format(
+            "Invalid hex color string %s. Colors must be in the "
+                + "form 'hhh' or 'hhhhhh'", hexColor));
+      }
+    }
+
     public Builder profileBackgroundColor(String profileBackgroundColor) {
+      validateHexColor(profileBackgroundColor);
       return parameter("profile_background_color", profileBackgroundColor);
     }
 
     public Builder profileTextColor(String profileTextColor) {
+      validateHexColor(profileTextColor);
       return parameter("profile_text_color", profileTextColor);
     }
 
     public Builder profileLinkColor(String profileLinkColor) {
+      validateHexColor(profileLinkColor);
       return parameter("profile_link_color", profileLinkColor);
     }
 
     public Builder profileSidebarFillColor(String profileSidebarFillColor) {
+      validateHexColor(profileSidebarFillColor);
       return parameter("profile_sidebar_fill_color", profileSidebarFillColor);
     }
 
     public Builder profileSidebarBorderColor(String profileSidebarBorderColor) {
+      validateHexColor(profileSidebarBorderColor);
       return parameter("profile_sidebar_border_color",
           profileSidebarBorderColor);
     }
 
     public UpdateProfileColorsRequest build() {
-      // Validate that: "Each parameter's value must be a valid hexidecimal 
-      // value, and may be either three or six characters (ex: #fff or #ffffff)."
+      if (!hasValid) {
+        throw new IllegalStateException(
+            "One or more of profileBackgroundColor, profileTextColor, "
+                + "profileLinkColor, profileSidebarFillColor, "
+                + "profileSidebarBorderColor must be set.");
+      }
       return new UpdateProfileColorsRequest(this);
     }
+
+    /**
+     * Returns true if the string matches the pattern hhh or hhhhhh.
+     * 
+     * @param hexColor A string of 3 or 6 hex characters.
+     * @return true if the string matches the pattern hhh or hhhhhh.
+     */
+    boolean isValidHexColor(String hexColor) {
+      return VALID_HEXCOLOR_PATTERN.matcher(hexColor).matches();
+    }
+
   }
 
   public User get() {
