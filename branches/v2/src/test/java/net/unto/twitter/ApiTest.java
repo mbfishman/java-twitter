1b4368fb9c37a3a546c3884a7eb73296c48a7a49
dclinton <dclinton@d718d8bd-922b-0410-8b2e-699bb0efa576>
2009-03-14 20:54:33 +0000

    Removed dead code

--- branches/v2/src/test/java/net/unto/twitter/ApiTest.java
+++ branches/v2/src/test/java/net/unto/twitter/ApiTest.java
@@ -1,18 +1,9 @@
 package net.unto.twitter;
 
-import static org.junit.Assert.*;
-import static org.easymock.EasyMock.*;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.CharBuffer;
-import java.util.List;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import net.unto.twitter.TwitterProtos.Device;
 import net.unto.twitter.TwitterProtos.Geocode;
-import net.unto.twitter.TwitterProtos.Status;
 import net.unto.twitter.UtilProtos.Url;
 import net.unto.twitter.UtilProtos.Url.Parameter;
 import net.unto.twitter.methods.Request;
@@ -423,61 +414,4 @@ public class ApiTest {
         expected));
   }
 
-
-
-  @Test
-  public void testGetPublicTimeline() throws IOException {
-    HttpManager mockTwitterHttpManager = getMockTwitterHttpManager();
-    Api api = Api.builder().httpManager(mockTwitterHttpManager).build();
-    String json = readTestData("public-timeline.json");
-    Url url = Url.newBuilder().setScheme(Url.Scheme.HTTP)
-        .setHost("twitter.com").setPort(80).setPath(
-            "/statuses/public_timeline.json").build();
-    expect(mockTwitterHttpManager.get(url)).andReturn(json);
-    replay(mockTwitterHttpManager);
-    List<Status> statuses = api.publicTimeline().build().get();
-    assertTrue(20 == statuses.size());
-    assertTrue(301231062L == statuses.get(0).getId());
-    assertEquals("I should sleep or else...", statuses.get(0).getText());
-    assertTrue(3188291L == statuses.get(0).getUser().getId());
-    verify(mockTwitterHttpManager);
-  }
-
-  @Test
-  public void testGetFriendsTimeline() throws IOException {
-    HttpManager mockTwitterHttpManager = getMockTwitterHttpManager();
-    Api api = Api.builder().httpManager(mockTwitterHttpManager).build();
-    String json = readTestData("friends-timeline-javaclient.json");
-    Url url = Url.newBuilder().setScheme(Url.Scheme.HTTP)
-        .setHost("twitter.com").setPort(80).setPath(
-            "/statuses/friends_timeline.json").build();
-    expect(mockTwitterHttpManager.get(url)).andReturn(json);
-    expect(mockTwitterHttpManager.hasCredentials()).andReturn(true);
-    replay(mockTwitterHttpManager);
-    List<Status> statuses = api.friendsTimeline().build().get();
-    assertTrue(1 == statuses.size());
-    assertTrue(303230492L == statuses.get(0).getId());
-    assertTrue(673483L == statuses.get(0).getUser().getId());
-    verify(mockTwitterHttpManager);
-  }
-
-  private String TEST_DATA_DIR = "src/test/data/";
-
-  private int MAX_TEST_DATA_FILE_SIZE = 16384;
-
-  private String readTestData(String fileName) throws IOException {
-    return readFromFile(new File(TEST_DATA_DIR, fileName));
-  }
-
-  private String readFromFile(File file) throws IOException {
-    Reader reader = new FileReader(file);
-    CharBuffer charBuffer = CharBuffer.allocate(MAX_TEST_DATA_FILE_SIZE);
-    reader.read(charBuffer);
-    charBuffer.position(0);
-    return charBuffer.toString();
-  }
-
-  private HttpManager getMockTwitterHttpManager() {
-    return createMock(HttpManager.class);
-  }
 }
