c9cd703999faf3dc07e13524990b167d9e66e762
dclinton <dclinton@d718d8bd-922b-0410-8b2e-699bb0efa576>
2009-03-14 20:55:25 +0000

    First pass at implementing JsonUtil tests.  Added new test data.  Removed old test data.

--- branches/v2/src/test/java/net/unto/twitter/JsonUtilTest.java
+++ branches/v2/src/test/java/net/unto/twitter/JsonUtilTest.java
@@ -1,6 +1,17 @@
 package net.unto.twitter;
 
 import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.CharBuffer;
+import java.util.List;
+
+import net.unto.twitter.TwitterProtos.Status;
+import net.unto.twitter.TwitterProtos.User;
 
 import org.junit.Test;
 
@@ -10,21 +21,35 @@ import org.junit.Test;
 public class JsonUtilTest {
 
   @Test
-  public void testNewUserList() {
-    fail("Not implemented");
+  public void testNewUserList() throws IOException {
+    String json = readTestData("user_list.json");
+    List<User> users = JsonUtil.newUserList(json);
+    assertEquals(new Integer(100), new Integer(users.size()));
   }
 
   @Test
-  public void testNewUser() {
-    fail("Not implemented");
+  public void testNewUser() throws IOException {
+    String json = readTestData("user.json");
+    User user = JsonUtil.newUser(json);
+    assertEquals("dewitt", user.getScreenName());
   }
 
+
   @Test
-  public void testNewStatus() {
-    fail("Not implemented");
+  public void testNewStatus() throws IOException {
+    String json = readTestData("status.json");
+    Status status = JsonUtil.newStatus(json);
+    assertEquals("☺", status.getText());
+    assertEquals("dewitt", status.getUser().getScreenName());
   }
   
-
+  @Test
+  public void testNewStatusList() throws IOException {
+    String json = readTestData("status_list.json");
+    List<Status> statuses = JsonUtil.newStatusList(json);
+    assertEquals(new Integer(100), new Integer(statuses.size()));
+  }
+  
   @Test
   public void testNewDirectMessageList() {
     fail("Not implemented");
@@ -58,4 +83,19 @@ public class JsonUtilTest {
     fail("Not implemented");
   }
   
+  private String TEST_DATA_DIR = "src/test/data/";
+
+  private int MAX_TEST_DATA_FILE_SIZE = 65536;
+
+  private String readTestData(String fileName) throws IOException {
+    return readFromFile(new File(TEST_DATA_DIR, fileName));
+  }
+
+  private String readFromFile(File file) throws IOException {
+    Reader reader = new FileReader(file);
+    CharBuffer charBuffer = CharBuffer.allocate(MAX_TEST_DATA_FILE_SIZE);
+    reader.read(charBuffer);
+    charBuffer.position(0);
+    return charBuffer.toString();
+  }
 }
