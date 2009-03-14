8ba93aaa89b1c1369804c88977cd72a11a98b012
dclinton@gmail.com <dclinton@gmail.com@d718d8bd-922b-0410-8b2e-699bb0efa576>
2009-03-14 22:53:13 +0000

    Implemented the remaining JsonUtil tests.

--- branches/v2/src/test/java/net/unto/twitter/JsonUtilTest.java
+++ branches/v2/src/test/java/net/unto/twitter/JsonUtilTest.java
@@ -10,7 +10,11 @@ import java.io.Reader;
 import java.nio.CharBuffer;
 import java.util.List;
 
+import net.unto.twitter.TwitterProtos.DirectMessage;
+import net.unto.twitter.TwitterProtos.RateLimitStatus;
+import net.unto.twitter.TwitterProtos.Results;
 import net.unto.twitter.TwitterProtos.Status;
+import net.unto.twitter.TwitterProtos.Trends;
 import net.unto.twitter.TwitterProtos.User;
 
 import org.junit.Test;
@@ -39,50 +43,62 @@ public class JsonUtilTest {
   public void testNewStatus() throws IOException {
     String json = readTestData("status.json");
     Status status = JsonUtil.newStatus(json);
-    assertEquals("☺", status.getText());
+    assertEquals("\u263A", status.getText());
     assertEquals("dewitt", status.getUser().getScreenName());
   }
-  
+
   @Test
   public void testNewStatusList() throws IOException {
     String json = readTestData("status_list.json");
     List<Status> statuses = JsonUtil.newStatusList(json);
-    assertEquals(new Integer(100), new Integer(statuses.size()));
-  }
-  
-  @Test
-  public void testNewDirectMessageList() {
-    fail("Not implemented");
-  }
-
-
-  @Test
-  public void testNewDirectMessage()  {
-    fail("Not implemented");
+    assertEquals(new Integer(20), new Integer(statuses.size()));
   }
 
   @Test
-  public void testNewLongArray() {
-    fail("Not implemented");
-  }
-  
-
-  @Test
-  public void testNewTrends() {
-    fail("Not implemented");
+  public void testNewDirectMessageList() throws IOException {
+    String json = readTestData("direct_messages.json");
+    List<DirectMessage> directMessages = JsonUtil.newDirectMessageList(json);
+    assertEquals(new Integer(2), new Integer(directMessages.size()));
   }
 
 
   @Test
-  public void testNewResults() {
-    fail("Not implemented");
+  public void testNewDirectMessage() throws IOException {
+    String json = readTestData("direct_message.json");
+    DirectMessage directMessage = JsonUtil.newDirectMessage(json);
+    assertEquals("dewitt", directMessage.getSender().getScreenName());
   }
 
   @Test
-  public void testNewRateLimitStatus() {
-    fail("Not implemented");
+  public void testNewLongArray() throws IOException {
+    String json = readTestData("ids.json");
+    long[] ids = JsonUtil.newLongArray(json);
+    assertEquals(new Integer(109), new Integer(ids.length));
   }
-  
+
+
+  @Test
+  public void testNewTrends() throws IOException {
+    String json = readTestData("trends.json");
+    Trends trends = JsonUtil.newTrends(json);
+    assertEquals(new Integer(10), new Integer(trends.getTrendsList().size()));
+  }
+
+
+  @Test
+  public void testNewResults() throws IOException {
+    String json = readTestData("search.json");
+    Results results = JsonUtil.newResults(json);
+    assertEquals(new Integer(15), new Integer(results.getResultsList().size()));
+  }
+
+  @Test
+  public void testNewRateLimitStatus() throws IOException {
+    String json = readTestData("rate_limit_status.json");
+    RateLimitStatus rateLimitStatus = JsonUtil.newRateLimitStatus(json);
+    assertEquals(new Integer(100), new Integer(rateLimitStatus.getHourlyLimit()));
+  }
+
   private String TEST_DATA_DIR = "src/test/data/";
 
   private int MAX_TEST_DATA_FILE_SIZE = 65536;
