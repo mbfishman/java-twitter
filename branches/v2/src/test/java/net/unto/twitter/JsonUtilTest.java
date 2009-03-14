5d1591eae535e73e27d0f9fb92a855fb966f2913
dclinton@gmail.com <dclinton@gmail.com@d718d8bd-922b-0410-8b2e-699bb0efa576>
2009-03-14 18:34:44 +0000

    Added test stubs for JsonUtil.

---
+++ branches/v2/src/test/java/net/unto/twitter/JsonUtilTest.java
@@ -0,0 +1,61 @@
+package net.unto.twitter;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for the JsonUtil class.
+ */
+public class JsonUtilTest {
+
+  @Test
+  public void testNewUserList() {
+    fail("Not implemented");
+  }
+
+  @Test
+  public void testNewUser() {
+    fail("Not implemented");
+  }
+
+  @Test
+  public void testNewStatus() {
+    fail("Not implemented");
+  }
+  
+
+  @Test
+  public void testNewDirectMessageList() {
+    fail("Not implemented");
+  }
+
+
+  @Test
+  public void testNewDirectMessage()  {
+    fail("Not implemented");
+  }
+
+  @Test
+  public void testNewLongArray() {
+    fail("Not implemented");
+  }
+  
+
+  @Test
+  public void testNewTrends() {
+    fail("Not implemented");
+  }
+
+
+  @Test
+  public void testNewResults() {
+    fail("Not implemented");
+  }
+
+  @Test
+  public void testNewRateLimitStatus() {
+    fail("Not implemented");
+  }
+  
+}
